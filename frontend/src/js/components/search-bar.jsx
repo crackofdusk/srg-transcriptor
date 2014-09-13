e1d19b6d8c55437c212966a541b96d9bb9e48442
Dimiter Petrov <crackofdusk@gmail.com>
2014-09-13 12:06:35 +0200

    Start implementing client side search

--- frontend/src/js/components/search-bar.jsx
+++ frontend/src/js/components/search-bar.jsx
@@ -2,11 +2,41 @@
 
 var React = require('react');
 
+var reqwest = require('reqwest');
+
 var SearchBar = React.createClass({
+  getInitialState: function() {
+    return {value: ''}
+  },
+
+  search: function(query) {
+    var results = reqwest({
+      url: "/search",
+      data: {
+        q: query
+      }
+    });
+
+    return results;
+  },
+
+  updateResults: function(results) {
+    console.log(results);
+  },
+
+  handleChange: function(event) {
+    var query = event.target.value;
+    this.setState({value: query});
+
+    this.search(query).then(this.updateResults);
+  },
+
   render: function() {
+    var value = this.state.value;
+
     return (
       <form>
-        <input type="text" placeholder="Search..." />
+        <input type="text" value={value} placeholder="Search..." onChange={this.handleChange} />
       </form>
     );
   }
